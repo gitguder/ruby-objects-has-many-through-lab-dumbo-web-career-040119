1f306bd69219b9bae4677dd6efe985569a3cf4e0
Codey Pinero <codey.pinero@gmail.com>
2019-04-10 20:23:46 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+require_relative 'song'
+require 'pry'
+
+class Artist
+
+  attr_accessor :genre, :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(song_name, genre)
+    Song.new(song_name, self, genre)
+  end
+
+  def songs
+  Song.all.select do |song|
+    song.artist == self
+  end
+end
+
+end
